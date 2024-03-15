2ecb32cc5cc763042737bc4db4e8fcf9b90019ee
MurungaOwen <owenhood80@gmail.com>
2024-03-15 18:36:21 +0300

    change function to check instance

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ import typing
 
 def to_kv(k : str,v : typing.Union[int, float]) -> tuple[str,float]:
     """get power of number and string"""  
-    return (k,float(math.pow(v,2)))
+    return k, v ** 2.0 if isinstance(v, (int, float)) else None
