e9c7d4f11776195bd162ba79bec1ca226d46a3a5
MurungaOwen <owenhood80@gmail.com>
2024-03-15 18:19:52 +0300

    rechange lines display in the module

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union
-import math
 """a type-annotated function to_kv that takes a string k and an int OR 
 float v as arguments and returns a tuple"""
+from typing import Union
+import math
 
 def to_kv(k : str,v : Union[int, float]) -> tuple:
     """a function takes a string k and an int OR float
--- 0x00-python_variable_annotations/README.md
+++ 0x00-python_variable_annotations/README.md
@@ -1,1 +1,2 @@
 # basic intro to annotations in python
+annotations in python helps us declare our variable or variables while giving the data type that they will be taking 
