921c0bbb9cb2c140c964abf64948a2a8c54ec7e6
MurungaOwen <owenhood80@gmail.com>
2024-03-15 18:31:21 +0300

    working on 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module containig to_kv"""
 import math
-from typing import Union
+import typing
 
-def to_kv(k : str,v : Union[int, float]) -> tuple[str,float]:
+def to_kv(k : str,v : typing.Union[int, float]) -> tuple[str,float]:
     """get power of number and string"""  
     return (k,float(math.pow(v,2)))
