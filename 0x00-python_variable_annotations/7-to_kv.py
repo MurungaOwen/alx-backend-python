123c5e0d6803bf61b4d8eb83530ee33e0e562b98
MurungaOwen <owenhood80@gmail.com>
2024-03-15 18:38:21 +0300

    refactor code

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module containig to_kv"""
 import math
-import typing
+from typing import Union
 
-def to_kv(k : str,v : typing.Union[int, float]) -> tuple[str,float]:
+def to_kv(k : str,v : Union[int, float]) -> tuple[str,float]:
     """get power of number and string"""  
-    return k, v ** 2.0 if isinstance(v, (int, float)) else None
+    return k,math.pow(v,2)
