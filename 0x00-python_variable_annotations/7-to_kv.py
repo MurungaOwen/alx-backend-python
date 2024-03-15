7072d5eb90294fce248580ce7b0b70130572c1bc
MurungaOwen <owenhood80@gmail.com>
2024-03-15 18:22:43 +0300

    change ks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-"""a type-annotated function to_kv that takes a string k and an int OR 
-float v as arguments and returns a tuple"""
-from typing import Union
+"""module containig to_kv"""
 import math
+from typing import Union
 
 def to_kv(k : str,v : Union[int, float]) -> tuple:
-    """a function takes a string k and an int OR float
-     v as arguments and returns a tuple. of string and square of number"""
-    
+    """get power of number and string"""  
     output = k
     output2 = float(math.pow(v,2))
     total = (output,output2)
