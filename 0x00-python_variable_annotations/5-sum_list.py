295f6ff13b3397e9f4fd949420090d60e7660e32
MurungaOwen <owenhood80@gmail.com>
2024-03-16 19:03:01 +0300

    lnit excercise 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,7 @@ returns their sum as a float."""
 
 def sum_list(input_list: List[float]) -> float:
     """function that adds a list of float values"""
-    sum: float = 0
+    sum = 0.0
     for i in input_list:
         sum += i
     return sum
