bbb0c5f69baa86eeb23ebdf5cd0dee089a23c470
MurungaOwen <owenhood80@gmail.com>
2024-03-18 18:33:49 +0300

    modify return type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """executing multiple coroutines"""
 import asyncio
+import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """async function to loop through and
         produce random input
         params:
