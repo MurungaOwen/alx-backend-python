be5af1f6786316cf20074d4e97e93a167045cea6
MurungaOwen <owenhood80@gmail.com>
2024-03-18 23:29:01 +0300

    modify annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,12 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """function uses random module to wait for a random delay
-    and returns it"""
+    and returns it
+    params:
+        max_delay wich is time to sleep
+    return:
+        return a random float
+    """
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
