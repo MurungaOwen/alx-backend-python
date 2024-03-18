ef7a441a19f88182622934d0e434efda6d1685df
MurungaOwen <owenhood80@gmail.com>
2024-03-18 18:27:43 +0300

    add return type

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,11 +3,12 @@
 Module containing function similar to
 wait_n
 """
+import typing
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """function that ges randm numbers
         in loop
         params:
